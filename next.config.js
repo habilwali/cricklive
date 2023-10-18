9b8d3c924a5fd37eabade4c5fa60c9ed0a6a6ad3
LAPTOP-ANCORTTM\habee <habeel497@gmail.com>
2023-10-18 11:55:18 +0400

    add domain path to images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    domains:['cricapp.bingerush.com']
+    domains:['cricapp.bingerush.com',
+    'sportsbizz.com'
+  ]
+    
       },
 }
 
