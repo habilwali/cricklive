da1f1a79d5230e50896041c41cacb14a63e63578
LAPTOP-ANCORTTM\habee <habeel497@gmail.com>
2023-11-30 15:20:34 +0400

    test

--- actions/get-commentary.js
+++ actions/get-commentary.js
@@ -3,7 +3,7 @@
 const URL=`${process.env.baseUrl}getMatchCommentary`;
 
 const getCommentary = async (id) => {
-  const res = await fetch(`${URL}/${id}`);
+  const res = await fetch(`${URL}/${id} `);
 
   return res.json();
 };
